ad61549b0f9b8cc59ac56fe93b131fc3a27627b7
Ogoegbunam Munaolisa Celestine <monaplayzsbackup@gmail.com>
2026-09-22 22:02:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    Current version: v1.0.0
    ============================================================ */
 
-const CACHE_VERSION = 'v1.0.2';
+const CACHE_VERSION = 'v1.0.3';
 const CACHE_NAME = 'cjaybudgets-' + CACHE_VERSION;
 
 // App files — stale-while-revalidate
